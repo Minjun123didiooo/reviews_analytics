8711a90e893f01b05911517f91becdd1ed2fcf26
Minjun <didioooc12@gmail.com>
2020-09-01 14:43:17 +0800

    new version

--- reviews.py
+++ reviews.py
@@ -1,12 +1,20 @@
 #寫程式碼來讀取留言檔案
-#印出總共幾筆
+#每讀一萬筆 印出len(data)來得知讀取進度
+#印出總共幾筆留言
 #利用索引印出特定留言
 
 data = []
+count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
+		count += 1
+		if count % 10000 == 0:
+			print(len(data))
+
+
 print(len(data))
+
 
 print(data[0])
 print('-------------------------------')
